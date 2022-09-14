1053c3505d3687fbc186b818e160ffe1caa5a4d9
Davepea <104210442+Davepea@users.noreply.github.com>
2022-09-14 10:47:43 -1100

    cartoon

---
+++ Cartoon/java.js
@@ -0,0 +1,29 @@
+var name = prompt("What is youe name")
+window.alert("name is " + name);
+console.log("Thanks" + name +"for coming")
+var number = prompt ("what is your age")
+window.alert("you are " + number +"years old")
+const days = 365;
+totaldays = days * number
+window.alert("you have used " + totaldays + " days on earth ")
+
+let a, b, c, d, e, f, g;
+a=250;
+b=250;
+c=10;
+d= a + b /c;
+e=1000;
+f=5;
+g=e*f;
+function myFunction(){document.getElementById('solve').innerHTML='250 + 250/10=' +' '+ d }
+function myCar(){document.getElementById('cool').innerHTML='Adebayo peace'}
+function myPop(){window.alert('SUNMENCE')}
+function myCole(){document.getElementById('school').innerHTML='1000 * 5 =' +' '+ g}
+function myGoal(){ document.getElementById('cola').innerHTML='Goodbye to Sunmence';}
+
+
+function myImage(){document.getElementById('img').src="rr.jpg"}
+function mySurname(){var name = prompt ("enter your surname") }
+function myFirstname(){var surname = prompt ("enter your Firstname") }
+function myAge(){var number = prompt ("enter your Age") }
+function myDetails(){window.alert("name is " + name)}
---
+++ Cartoon/jscon.js
@@ -0,0 +1,20 @@
+var surname, Firstname, age; 
+function mySurname(){
+    surname = prompt ("enter your surname") }
+function myFirstname(){
+    Firstname= prompt ("enter your Firstname") }
+function myAge(){
+    age = prompt ("enter your Age") }
+const shoe = number
+function myDetails(){document.getElementById('details').innerHTML='name is '  + Firstname +' '+ surname +''+ ',    Age is ' + age }
+function myShoe(){
+const  d = new Date().getHours();
+let greetings;
+if(d <= 2){
+    greetings=alert('Goodmorning')
+} 
+else if(d <= 10){
+    greetings=alert('Goodafternoon')
+}
+
+}
